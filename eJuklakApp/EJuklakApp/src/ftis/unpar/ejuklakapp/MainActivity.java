d073a1e75e6c0a452f7e49f288b5c708f00934ef
astridsoraya <astridsoraya77@gmail.com>
2015-04-23 13:21:57 +0700

    Penambahan tag Lampiran di Navigation Drawer

--- eJuklakApp/EJuklakApp/src/ftis/unpar/ejuklakapp/MainActivity.java
+++ eJuklakApp/EJuklakApp/src/ftis/unpar/ejuklakapp/MainActivity.java
@@ -58,31 +58,10 @@ NavigationDrawerFragment.NavigationDrawerCallbacks {
 				(DrawerLayout) findViewById(R.id.drawer_layout));
 		
 		webView = (WebView) findViewById(R.id.webview);
-		webView.setWebViewClient(new WebViewClient(){
-		    
-			@Override
-		    public void onLoadResource(WebView view, String url){
-		    	String urlLampiran = getResources().getString(R.string.picture_html);
-		    	String[] namaGambar = getResources().getStringArray(R.array.picture_array);
-		    	
-	        	view.getSettings().setBuiltInZoomControls(false);
-	        	view.getSettings().setLoadWithOverviewMode(false);
-    			view.getSettings().setUseWideViewPort(false);
-	        	
-	        	
-		    	for(int i = 0; i < namaGambar.length; i++){
-		    		if(url.equals(urlLampiran+namaGambar[i])){
-		    			view.getSettings().setLoadWithOverviewMode(true);
-		    			view.getSettings().setUseWideViewPort(true);
-		    			view.getSettings().setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
-			        	view.getSettings().setBuiltInZoomControls(true);
-		                openPicture();
-		                i += namaGambar.length;
-		    		}
-		    	}
-
-		    }
-		});
+		webView.getSettings().setLoadWithOverviewMode(true);
+		webView.getSettings().setUseWideViewPort(true);
+		webView.getSettings().setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
+    	webView.getSettings().setBuiltInZoomControls(true);
 		
 		
 	}
@@ -196,55 +175,7 @@ NavigationDrawerFragment.NavigationDrawerCallbacks {
 		else{
 			super.onBackPressed();
 		}
-		//mNavigationDrawerFragment.mDrawerLayout.isDrawerOpen(Gravity.LEFT);
-		/*//super.onBackPressed();
-		if(mNavigationDrawerFragment.isDrawerOpen(Gravity.LEFT))
-		{
-			mNavigationDrawerFragment.mDrawerLayout.setFocusableInTouchMode(false);
-		}
-		mNavigationDrawerFragment.mDrawerLayout.setFocusableInTouchMode(false);*/
-//		restoreActionBar();
 		
-	}
-	
-	/**
-	* A placeholder fragment containing a simple view.
-	*/
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-			private static final String ARG_SECTION_NUMBER = "section_number";
-		
-		/**
-		 * Returns a new instance of this fragment for the given section number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-		
-		public PlaceholderFragment() {
-		}
-		
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-		
-		@Override
-		public void onAttach(Activity activity) {
-			super.onAttach(activity);
-			((MainActivity) activity).onSectionAttached(getArguments().getInt(
-					ARG_SECTION_NUMBER));
-		}
 	}
 
 }
